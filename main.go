7813bf8e520ef8cc125ac144d10fc939646def1a
kita127 <kitada.kosuke@gmail.com>
2020-05-07 15:20:04 +0900

    ワンショットモードとインタラクティブモードに分けた

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"log"
 	"os"
 	"time"
@@ -13,19 +14,14 @@ import (
 )
 
 var (
-	portFlag = kingpin.Flag("port", "port name (--port=COM3)").Required().String()
-	baudRate = kingpin.Flag("baud-rate", "baud rate (--baud-rate=9600)").Default("9600").Int()
-	readTime = kingpin.Flag("read-time", "read cycle time(ms)").Default("100").Int()
+	portFlag    = kingpin.Flag("port", "port name (--port=COM3)").Required().String()
+	baudRate    = kingpin.Flag("baud-rate", "baud rate (--baud-rate=9600)").Default("9600").Int()
+	readTime    = kingpin.Flag("read-time", "read cycle time(ms)").Default("100").Int()
+	interactive = kingpin.Flag("interactive", "interactive mode").Short('i').Bool()
 )
 
 func main() {
 	kingpin.Parse()
-
-	// 全部読み
-	//text, err := ioutil.ReadAll(os.Stdin)
-	//if err != nil {
-	//	log.Fatal(err)
-	//}
 
 	// Set up options.
 	options := serial.OpenOptions{
@@ -44,6 +40,24 @@ func main() {
 
 	// Make sure to close it later.
 	defer port.Close()
+
+	if *interactive {
+		// interactive mode
+		interactiveMode(port)
+	} else {
+		// one-shot mode
+		text, err := ioutil.ReadAll(os.Stdin)
+		if err != nil {
+			log.Fatal(err)
+		}
+		_, err = port.Write([]byte(text))
+		if err != nil {
+			log.Fatalf("port.Write: %v", err)
+		}
+	}
+}
+
+func interactiveMode(port io.ReadWriteCloser) {
 
 	go func() {
 		t1 := time.NewTicker(time.Duration(*readTime) * time.Millisecond)
@@ -75,7 +89,7 @@ func main() {
 		for f.Scan() {
 			text := f.Text()
 			text = text + "\n"
-			_, err = port.Write([]byte(text))
+			_, err := port.Write([]byte(text))
 			if err != nil {
 				log.Fatalf("port.Write: %v", err)
 			}
